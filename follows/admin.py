2dc0e80bda813e00644f7e3146ed91b789ac7392
Ali Zain Malik <alizainkmalik@gmail.com>
2026-03-22 23:40:58 -0600

    Register FollowRelationship in Django admin

--- follows/admin.py
+++ follows/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import FollowRelationship
 
-# Register your models here.
+@admin.register(FollowRelationship)
+class FollowRelationshipAdmin(admin.ModelAdmin):
+    list_display = ("follower", "followee", "status", "created_at")
+    list_filter = ("status",)
+    search_fields = ("follower__display_name", "followee__display_name")
