b65a764196a542566eb2f855f2d2eea352bde0c1
Regan <reganvrn@gmail.com>
2026-03-30 00:25:15 -0600

    fixed 1 author edge case

--- follows/views.py
+++ follows/views.py
@@ -102,7 +102,7 @@ def follow_ui_page(request: HttpRequest) -> HttpResponse:
         .order_by("display_name")
     )
 
-    if not local_authors.is_empty():
+    if local_authors:
         for x in reversed(range(len(local_authors))):
             authorObj = AuthorAccount.objects.get(author=local_authors[x])
             user = authorObj.user
